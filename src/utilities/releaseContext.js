a83eb4749c4544418903353c52f0225da2136545
James Smith <james.smith@djalbat.com>
2026-05-09 20:37:35 +0100

    More slight architectural changes.

--- src/utilities/releaseContext.js
+++ src/utilities/releaseContext.js
@@ -1,17 +1,12 @@
 "use strict";
 
 import { Entries, metaJSONUtilities } from "occam-model";
-import { fileSystemUtilities as occamFileSystemUtilities } from "occam-server";
-import { pathUtilities, fileSystemUtilities as necessaryFileSystemUtilities } from "necessary";
 
 import ReleaseContext from "../context/release";
 
 import { customGrammarFromNameAndEntries } from "../utilities/customGrammar";
 
-const { loadProject } = occamFileSystemUtilities,
-      { concatenatePaths } = pathUtilities,
-      { isMetaJSONFileValid } = metaJSONUtilities,
-      { readFile, isEntryFile, checkEntryExists } = necessaryFileSystemUtilities;
+const { isMetaJSONFileValid } = metaJSONUtilities;
 
 export function releaseContextFromJSON(json, context) {
   const { log, callback } = context,
@@ -70,35 +65,6 @@ export function releaseContextFromRelease(release, context) {
           customGrammar = customGrammarFromNameAndEntries(name, entries);
 
     releaseContext = ReleaseContext.fromLogNameJSONEntriesCallbackAndCustomGrammar(log, name, json, entries, callback, customGrammar);
-  }
-
-  return releaseContext;
-}
-
-export async function releaseContextFromDependency(dependency, context) {
-  let releaseContext = null;
-
-  const { projectsDirectoryPath } = context,
-        dependencyName = dependency.getName(),
-        entryPath = concatenatePaths(projectsDirectoryPath, dependencyName),
-        entryExists = checkEntryExists(entryPath);
-
-  if (entryExists) {
-    const entryFile = isEntryFile(entryPath);
-
-    if (entryFile) {
-      const filePath = entryPath, ///
-            content = readFile(filePath),
-            jsonString = content, ///
-            json = JSON.parse(jsonString);
-
-      releaseContext = releaseContextFromJSON(json, context);
-    } else {
-      const projectName = dependencyName, ///
-            project = loadProject(projectName, projectsDirectoryPath);
-
-      releaseContext = releaseContextFromProject(project, context);
-    }
   }
 
   return releaseContext;
