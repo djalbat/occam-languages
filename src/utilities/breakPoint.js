25dd87a89ece6b1e5c0aabeba288f256cdb5fecc
James Smith <james.smith@djalbat.com>
2026-07-06 20:49:20 +0100

    Fixed the line index function.

--- src/utilities/breakPoint.js
+++ src/utilities/breakPoint.js
@@ -31,21 +31,19 @@ export function breakPointToBreakPointJSON(breakPoint) {
 export function lineIndexFromNodeAndTokens(node, tokens) {
   let lineIndex = 0;
 
-  // const firstSignificantTokenIndex = node.getFirstSignificantTokenIndex(tokens);
-  //
-  // tokens.some((token, tokenIndex) => {
-  //   const tokenEndOfLineToken = token.isEndOfLineToken();
-  //
-  //   if (tokenEndOfLineToken) {
-  //     lineIndex += 1;
-  //   }
-  //
-  //   if (tokenIndex === firstSignificantTokenIndex) {
-  //     return true;
-  //   }
-  // });
+  const firstSignificantTokenIndex = node.getFirstSignificantTokenIndex(tokens);
 
-  lineIndex = 123;
+  tokens.some((token, tokenIndex) => {
+    const tokenEndOfLineToken = token.isEndOfLineToken();
+
+    if (tokenEndOfLineToken) {
+      lineIndex += 1;
+    }
+
+    if (tokenIndex === firstSignificantTokenIndex) {
+      return true;
+    }
+  });
 
   return lineIndex;
 }
